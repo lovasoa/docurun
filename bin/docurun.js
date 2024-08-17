1a8aa7dbfc2438bbde2bb2ea2290dc5e0949e063
lovasoa <contact@ophir.dev>
2024-08-17 13:05:00 +0200

    fix https://github.com/lovasoa/docurun/discussions/2

--- bin/docurun.js
+++ bin/docurun.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 import { run_in_folder } from '../cli.js'
+import { join } from 'path'
 
 let source_folder = process.env.DOCURUN_SOURCE_FOLDER || './docurun/';
-let destinatination_folder = process.env.DOCURUN_DESTINATION_FOLDER || './docurun/website/';
+let destinatination_folder = process.env.DOCURUN_DESTINATION_FOLDER || join(source_folder, 'website');
 
 run_in_folder(source_folder, destinatination_folder)
